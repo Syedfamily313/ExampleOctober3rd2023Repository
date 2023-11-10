5299ee8169eaa125fd43f72b318d7bba23782831
Syedfamily313 <syedmohiuddin22011997@gmail.com>
2023-11-10 17:37:53 +0530

    Sample Programme

---
+++ JavaPracticeProject/src/p3/Demo1.java
@@ -0,0 +1,13 @@
+package p3;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+// Display 10th table
+		for(int i=1; i<10; i++)
+		{
+			System.out.println("10 *"+i+" = "+10*i);
+		}
+	}
+
+}
