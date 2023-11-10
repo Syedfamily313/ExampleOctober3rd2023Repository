b4c4fae0cda75d02e4fd4a5bdaded5e9d71874c8
Syedfamily313 <syedmohiuddin22011997@gmail.com>
2023-11-10 17:59:10 +0530

    Test Program

---
+++ JavaPracticeProject/src/p3/Test.java
@@ -0,0 +1,13 @@
+package p3;
+
+public class Test {
+
+	public static void main(String[] args) {
+    //display 1 to 10 order 
+		for(int i=1; i<=10; i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
